9eff8d8116b9d769e9f5e3d9cbd3634cfcdd91c8
anujk14 <khandelwal.anuj14@gmail.com>
2016-10-27 23:54:10 +0530

    Created function for addition
    Solution for issue #5

--- add.java
+++ add.java
@@ -1,11 +1,15 @@
 import java.util.*;
 
 class BasicPrograms{
+  public static int add(int a, int b){
+    return(a+b); 
+  }
+  
   public static void main(String args[]){
     Scanner ask=new Scanner(System.in);
     int a = ask.nextInt();
     int b = ask.nextInt();
-    int sum = a+b;
+    int sum = add(a,b);
     System.out.println("Addition: "+sum);
   }
 }
