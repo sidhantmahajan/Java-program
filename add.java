ce6c45079d7c01d6ed68d2876f88416e888d4c3c
anujk14 <khandelwal.anuj14@gmail.com>
2016-10-27 23:48:51 +0530

    Created program for addition
    Solution for issue #3

---
+++ add.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class BasicPrograms{
+  public static void main(String args[]){
+    Scanner ask=new Scanner(System.in);
+    int a = ask.nextInt();
+    int b = ask.nextInt();
+    int sum = a+b;
+    System.out.println("Addition: "+sum);
+  }
+}
