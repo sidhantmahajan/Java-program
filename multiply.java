c8a8beca6e8ffc1045cf8dc22f3e4dd0ac1ba46c
anujk14 <khandelwal.anuj14@gmail.com>
2016-10-27 23:59:09 +0530

    Created program for multiplication
    Solution for issue #7

---
+++ multiply.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class BasicPrograms{
+  public static int multiply(int a, int b){
+    return(a8b); 
+  }
+  
+  public static void main(String args[]){
+    Scanner ask=new Scanner(System.in);
+    int a = ask.nextInt();
+    int b = ask.nextInt();
+    int product = multiply(a,b);
+    System.out.println("Product: "+product);
+  }
+}
