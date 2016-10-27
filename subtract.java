590b12f225d6d187271e5168b80eb1f17b7b83d8
Ashwini Purohit <ashwinipurohit@yahoo.in>
2016-10-28 00:41:27 +0530

    Create subtract.java

---
+++ subtract.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class BasicPrograms{
+  public static int subtract(int a, int b){
+    return(a-b); 
+  }
+  
+  public static void main(String args[]){
+    Scanner ask=new Scanner(System.in);
+    int a = ask.nextInt();
+    int b = ask.nextInt();
+    int difference = subtract(a,b);
+    System.out.println("Subtraction: "+sum);
+  }
+}
