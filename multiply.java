e549f19f12e3171765a4cd4f9a82da742cf08145
Mojorhys <mahasid.sidhant@gmail.com>
2017-11-01 11:06:11 +0530

    Update multiply.java

--- multiply.java
+++ multiply.java
@@ -1,15 +1,23 @@
-import java.util.*;
+import java.util.Scanner; //Need to import this to use the Scanner object
 
-class BasicPrograms{
-  public static int multiply(int a, int b){
-    return(a*b); 
-  }
+class AddNumbers
+{
   
   public static void main(String args[]){
-    Scanner ask=new Scanner(System.in);
-    int a = ask.nextInt();
-    int b = ask.nextInt();
-    int product = multiply(a,b);
-    System.out.println("Product: "+product);
+    int x; //Decalre integer variable x
+    int y; //Declare integer variable y
+    int sum; //Declare integer variable sum
+
+    //Use System.out.println to print to console
+    System.out.println("Enter two integers to calculate their sum ");
+
+    //Scanner System.in allows for user input
+    Scanner in = new Scanner(System.in);
+    x = in.nextInt(); //First input goes into variable x
+    y = in.nextInt(); //Second input goes into variable y
+    mul = x * y; //x and y get multiplied together and their value is stored into sum
+
+    //Print out the answer || use '+' when you want to print more on the same line
+    System.out.println("Multiplication of entered integers = " + mul);
   }
 }
